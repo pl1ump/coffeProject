a2d0e896746a9a2c1a41415f451b1fb8f2a0a797
vladyslav <nnnnnniikttttto@gmail.com>
2025-10-30 18:21:08 +0100

    initial commit

---
+++ coffeeProject/coffeeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffeeProjectApp.swift
+//  coffeeProject
+//
+//  Created by Vladick  on 30/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct coffeeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
