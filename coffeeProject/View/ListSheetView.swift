132c18ffc53238afae2078ade62d620c8c3cdf83
vladyslav <nnnnnniikttttto@gmail.com>
2025-11-04 20:31:34 +0100

    create rest views

---
+++ coffeeProject/View/ListSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  ListSheetView.swift
+//  coffeeProject
+//
+//  Created by Vladick  on 04/11/2025.
+//
+
+import SwiftUI
+
+struct ListSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListSheetView()
+}
---
+++ coffeeProject/View/MapMarker.swift
@@ -0,0 +1,18 @@
+//
+//  MapMarker.swift
+//  coffeeProject
+//
+//  Created by Vladick  on 03/11/2025.
+//
+
+import SwiftUI
+
+struct MapMarker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapMarker()
+}
