b4e6b8d72702144ba67d505f9371cb1ad57ef3b6
vladyslav <nnnnnniikttttto@gmail.com>
2025-11-11 22:34:12 +0100

    edit files

--- coffeeProject/View/MapSearchView.swift
+++ coffeeProject/View/MapSearchView.swift
@@ -4,7 +4,7 @@ import MapKit
 struct MapSearchView<ViewModel: MapViewModelProtocol>: View {
     @StateObject private var viewModel: ViewModel
     
-    @State private var addressInput: String = ""
+    @State private var adressInput: String = ""
     @State private var showFilters = false
     @State private var previousRadius: Int = 0
     @State private var showCoffeeList = false
@@ -35,33 +35,15 @@ struct MapSearchView<ViewModel: MapViewModelProtocol>: View {
             }
             .ignoresSafeArea()
             
-            
-            VStack {
-                HStack {
-                    TextField(LocalizedStringKey("Enter office address…"), text: $addressInput)
-                        .padding()
-                        .background(Color(.systemBackground))
-                        .cornerRadius(12)
-                        .shadow(radius: 4)
-                        .onSubmit {
-                            Task { await viewModel.searchAddress(addressInput) }
-                        }
-                    
-                    Button(action: {
-                        showFilters.toggle()
-                    }) {
-                        Image(systemName: "line.3.horizontal.decrease.circle")
-                            .font(.title2)
-                            .padding(8)
-                            .background(Color(.systemBackground))
-                            .cornerRadius(12)
-                            .shadow(radius: 4)
-                    }
+            SearchBarView(adressInput: $adressInput) {
+                Task {
+                    await viewModel.searchAddress(adressInput)
                 }
-                .padding()
-                
-                Spacer()
+            } onSubmit: {
+                showFilters.toggle()
             }
+
+            
         }
         .sheet(isPresented: $showFilters, onDismiss: {
             if viewModel.searchRadius != previousRadius {
--- coffeeProject/View/SearchBarView.swift
+++ coffeeProject/View/SearchBarView.swift
@@ -8,11 +8,43 @@
 import SwiftUI
 
 struct SearchBarView: View {
+    @Binding var adressInput: String
+    let onFilterTap: () -> Void
+    let onSubmit: () -> Void
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                TextField(LocalizedStringKey("Enter office address…"), text: $adressInput)
+                    .onChange(of: adressInput) { _, newValue in
+                        adressInput = newValue
+                            .trimmingCharacters(in: .whitespacesAndNewlines)
+                            .replacingOccurrences(of: "  ", with: " ")
+                            .prefix(60)
+                            .description
+                    }
+                    .padding()
+                    .background(Color(.systemBackground))
+                    .cornerRadius(12)
+                    .shadow(radius: 4)
+                    .onSubmit {
+                        onSubmit()
+                    }
+                
+                Button(action: {
+                    onFilterTap()
+                }) {
+                        Image(systemName: "line.3.horizontal.decrease.circle")
+                            .font(.title2)
+                            .padding(8)
+                            .background(Color(.systemBackground))
+                            .cornerRadius(12)
+                            .shadow(radius: 4)
+                    }
+            }
+            .padding()
+            
+            Spacer()
+        }
     }
 }
 
-#Preview {
-    SearchBarView()
-}
