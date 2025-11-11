5edeede1025c2d4ed30f3ae0d406ae688654781e
vladyslav <nnnnnniikttttto@gmail.com>
2025-11-11 22:34:01 +0100

    create bar view file

---
+++ coffeeProject/View/SearchBarView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBarView.swift
+//  coffeeProject
+//
+//  Created by Vladick  on 11/11/2025.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBarView()
+}
